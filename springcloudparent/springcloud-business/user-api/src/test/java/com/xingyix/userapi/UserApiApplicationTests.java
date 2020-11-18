29baa881d329df1cebc28de75eaf0d880ce61928
dengsk <dengsk@998.com>
2020-11-18 17:48:57 +0800

    添加初始项目

---
+++ springcloudparent/springcloud-business/user-api/src/test/java/com/xingyix/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyix.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
